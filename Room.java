f5e3f6940260adc53d7c2e1b1f58996b96c9ee76
Tvé Jméno <liskaondra2@gmail.com>
2025-03-04 18:32:35 +0100

    První commit

---
+++ Room.java
@@ -0,0 +1,33 @@
+public class Room {
+    private final int index;
+    private final String name;
+    private final List<Integer> neighbors;
+
+    public Room(int index, String name, List<Integer> neighbors) {
+        this.index = index;
+        this.name = name;
+        this.neighbors = neighbors;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getNeighbors() {
+        return neighbors;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "index=" + index +
+                ", name='" + name + '\'' +
+                ", neighbors=" + neighbors +
+                '}';
+    }
+}
+
